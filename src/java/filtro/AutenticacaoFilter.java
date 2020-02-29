9c12ff2552e65bb5404af67cb727adc379d8e60e
Alexandre <leschotfrederick@gmail.com>
2020-02-29 12:31:51 -0300

    Adicionado filtro / novas páginas de erro 404,500.

---
+++ src/java/filtro/AutenticacaoFilter.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filtro;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Usuario;
+
+/**
+ *
+ * @author Aluno 04
+ */
+@WebFilter(filterName = "AutenticacaoFilter", urlPatterns = {"/home"}) // o filtro só se aplica a esta página (para aplicar,se a mais, basta colocar , "/caminho" ou /*
+public class AutenticacaoFilter implements Filter {
+    
+    private static final boolean debug = true;
+
+    // The filter configuration object we are associated with.  If
+    // this value is null, this filter instance is not currently
+    // configured. 
+    private FilterConfig filterConfig = null;
+    
+    public AutenticacaoFilter() {
+    }    
+  
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        HttpSession sessao = httpRequest.getSession();
+        Usuario u = (Usuario) sessao.getAttribute("usuarioLogado");
+        boolean requerAutenticacao = true;
+        
+        if(u == null && requerAutenticacao){
+            httpResponse.sendRedirect("index.jsp");
+            return;
+        }
+        
+    }
+
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+
+   
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    
+    public void destroy() {        
+    }
+
+   
+    public void init(FilterConfig filterConfig) {        
+        this.filterConfig = filterConfig;
+        if (filterConfig != null) {
+            if (debug) {                
+                log("AutenticacaoFilter:Initializing filter");
+            }
+        }
+    }
+
+    /**
+     * Return a String representation of this object.
+     */
+    @Override
+    public String toString() {
+        if (filterConfig == null) {
+            return ("AutenticacaoFilter()");
+        }
+        StringBuffer sb = new StringBuffer("AutenticacaoFilter(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+    
+    private void sendProcessingError(Throwable t, ServletResponse response) {
+        String stackTrace = getStackTrace(t);        
+        
+        if (stackTrace != null && !stackTrace.equals("")) {
+            try {
+                response.setContentType("text/html");
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                PrintWriter pw = new PrintWriter(ps);                
+                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
+
+                // PENDING! Localize this for next official release
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");                
+                pw.print(stackTrace);                
+                pw.print("</pre></body>\n</html>"); //NOI18N
+                pw.close();
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        } else {
+            try {
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                t.printStackTrace(ps);
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+    
+    public static String getStackTrace(Throwable t) {
+        String stackTrace = null;
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+            pw.close();
+            sw.close();
+            stackTrace = sw.getBuffer().toString();
+        } catch (Exception ex) {
+        }
+        return stackTrace;
+    }
+    
+    public void log(String msg) {
+        filterConfig.getServletContext().log(msg);        
+    }
+    
+}
--- src/java/servlet/HomeServlet.java
+++ src/java/servlet/HomeServlet.java
@@ -20,6 +20,16 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession sessao = request.getSession();
+        Usuario usuarioLogado = (Usuario) sessao.getAttribute("Usuario");
+
+        
+//        if (usuarioLogado == null) {
+////            o usuário não está logado, portanto redirecioná-lo para index.jsp
+//            response.sendRedirect("index.jsp");
+//            return;
+//        } 
+        
 
     }
 
@@ -27,46 +37,44 @@ public class HomeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession sessao = request.getSession();
-        Usuario usuarioLogado = (Usuario)sessao.getAttribute("Usuario");
+        Usuario usuarioLogado = (Usuario) sessao.getAttribute("Usuario");
 
         String acao = request.getParameter("acao");
 
-        
-        if(acao.equals("adicionar")){
-            if(TarefaDAO.inserirTarefa(usuarioLogado, request.getParameter("tituloTarefa")) >0){
-            request.setAttribute("atualizado", "Tarefa adicionada com sucesso!");
-            RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
-            rd.forward(request, response);
-            return;
-            }else{
+        if (acao.equals("adicionar")) {
+            if (TarefaDAO.inserirTarefa(usuarioLogado, request.getParameter("tituloTarefa")) > 0) {
+                request.setAttribute("atualizado", "Tarefa adicionada com sucesso!");
+                RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+                rd.forward(request, response);
+                return;
+            } else {
                 request.setAttribute("atualizado", "Ocorreu um erro ao adicionar a tarefa!");
-            RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
-            rd.forward(request, response);
-            return;
-            }
-                
-        }
-        if(acao.equals("listar")){
-            ArrayList<Tarefa> tarefas = TarefaDAO.listarTarefas(usuarioLogado);
-            request.setAttribute("listado", tarefas);
-            
-            RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
-            rd.forward(request, response);
-
-            return;
-        }
-        
-        if (acao.equals("alterar")) {
-            String novoEmail = request.getParameter("novoEmail");
-            String novaSenha = request.getParameter("novaSenha");
-            if (novaSenha.length() < 5 || novaSenha.length() > 20) {
-                request.setAttribute("atualizado","Ocorreu um erro ao alterar os dados");
-                request.setAttribute("erroSenha", "A senha deve ter entre 5 e 20 caracteres");
                 RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
                 rd.forward(request, response);
                 return;
             }
-            else if (UsuarioDAO.atualizarUsuario(usuarioLogado, novoEmail, novaSenha) > 0) {
+
+        }
+        if (acao.equals("listar")) {
+            ArrayList<Tarefa> tarefas = TarefaDAO.listarTarefas(usuarioLogado);
+            request.setAttribute("listado", tarefas);
+
+            RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+            rd.forward(request, response);
+
+            return;
+        }
+
+        if (acao.equals("alterar")) {
+            String novoEmail = request.getParameter("novoEmail");
+            String novaSenha = request.getParameter("novaSenha");
+            if (novaSenha.length() < 5 || novaSenha.length() > 20) {
+                request.setAttribute("atualizado", "Ocorreu um erro ao alterar os dados");
+                request.setAttribute("erroSenha", "A senha deve ter entre 5 e 20 caracteres");
+                RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+                rd.forward(request, response);
+                return;
+            } else if (UsuarioDAO.atualizarUsuario(usuarioLogado, novoEmail, novaSenha) > 0) {
                 request.setAttribute("atualizado", "Email e senha atualizados com sucesso!");
                 RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
                 rd.forward(request, response);
@@ -84,7 +92,7 @@ public class HomeServlet extends HttpServlet {
                 return;
 
             }
-            
+
         }
     }
 
